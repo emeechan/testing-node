1ccbd250d106782614ed569a0c6075d212404a5e
Emily Meechan <emilymeechan1@gmail.com>
2024-03-14 11:44:09 +0000

    jxs experimenting

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,9 @@
 function App() {
     return (
-        <h1>Hello from the app component</h1>
+        <div className='container'>
+        <h1>My App</h1>
+        <p>HELLO</p>
+        </div>
     )
 }
 
