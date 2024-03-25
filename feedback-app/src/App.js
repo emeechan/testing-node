a37f1d610c741bfce8c42b29014c011880e21d43
Emily Meechan <emilymeechan1@gmail.com>
2024-03-25 14:35:56 +0000

    adding in props

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Header';
 function App() {
     return (
         <>
-        <Header />
+        <Header text />
         <div className='container'>
             <h1>My App</h1>
     </div>
