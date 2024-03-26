56c0465840ac8e60e45f9f42d15c55542942e715
Emily Meechan <emilymeechan1@gmail.com>
2024-03-26 14:58:39 +0000

    adding feedback items using global state (FeedbackData.js wouldnt link so coded it in app.js instead

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,14 +1,36 @@
 import './index.css'
-
+import { useState } from 'react'
 import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
 
 function App() {
+    //app level state
+    //[const name, function to edit state]
+    const [feedback, setFeedback] = useState([
+        {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        ])
+
+
+
     return (
         <>
         <Header />
         <div className='container'>
-            <FeedbackItem />
+            <FeedbackList feedback={feedback}/>
     </div>
     </>
  )
--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,25 +1,8 @@
-// example of a hook is useState
-import {useState} from 'react'
-
-function FeedbackItem() {
-    //setting a piece of state
-    //brackets holds the default for that piece of state
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example of a feedback item.')
-
-  //adds on to rating each time button is clicked
-    const handleClick = () => {
-    setRating((prev) => {
-        return prev + 1
-    })
-  }
-  
+function FeedbackItem({item}) {
     return (
-    //when button is clicked, call the handleClick function
     <div className='card'>
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   )
 }
