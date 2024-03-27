7e0d3cebccc74016c31ac5e302d80a25cf7c3d41
Emily Meechan <emilymeechan1@gmail.com>
2024-03-27 14:01:15 +0000

    rating selection component 1-10

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
+import RatingSelect from "./RatingSelect"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
     //until they type 10 characters, the button is disabled
     const [btnDisabled, setBtnDisabled] = useState(true)
     //displays the message of the minimum character count
@@ -32,7 +34,7 @@ function FeedbackForm() {
     <Card>
       <from>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)}/>
         <div className='input-group'>
             <input 
             onChange={handleTextChange}
