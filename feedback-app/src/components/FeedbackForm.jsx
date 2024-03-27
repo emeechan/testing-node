91f06248468878f66cf8ef5c8aa5c4d24816c253
Emily Meechan <emilymeechan1@gmail.com>
2024-03-27 13:35:52 +0000

    uses real time validation to determine the length of a review being written and only allows submition if >10 characters

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -4,9 +4,27 @@ import Button from "./shared/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    //until they type 10 characters, the button is disabled
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    //displays the message of the minimum character count
+    const [message, setMessage] = useState('')
 
     //gets what the user is typing
     const handleTextChange = (e) => {
+        //if no text then disable the button
+        if(text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+            //if there is text but its less than 10 characters, then disable the button
+        }   else if(text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters')
+            setBtnDisabled(true)
+            //else if it is 10 characters then dont display the message and enable the button
+        }   else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
     setText(e.target.value)
 }
 
@@ -21,9 +39,10 @@ function FeedbackForm() {
             type={text}
             placeholder="Write a review" />
             {/* brings in the Button component */}
-            <Button type="submit">Send</Button>
+            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
 
+        {message && <div className='message'>{message}</div>}
       </from>
     </Card>
   )
